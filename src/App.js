dd142798452c08807c34fb906b688901f537a7a0
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2025-07-26 15:23:52 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { useState } from 'react';
+
 function App() {
   return (
     <div className="App">
       <h2>hello, this is ololade</h2>
+      <Counter></Counter>
+    </div>
+  );
+}
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((step) => step - 1)}>-</button>
+        <span>step:{step}</span>
+        <button onClick={() => setStep((step) => step + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((count) => count - step)}>-</button>
+        <span>count:{count}</span>
+        <button onClick={() => setCount((count) => count + step)}>+</button>
+      </div>
+      <span>
+        {count === 0
+          ? 'Today is '
+          : count > 0
+          ? `${count} days from today is`
+          : `${Math.abs(count)}  days ago was`}
+      </span>
+      <span> {date.toDateString()}</span>
     </div>
   );
 }
